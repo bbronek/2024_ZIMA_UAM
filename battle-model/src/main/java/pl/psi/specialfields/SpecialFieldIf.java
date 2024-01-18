902da92840c585a696db8da9d2580dc78dcdcdb9
bbronek <barbro214@gmail.com>
2024-01-18 21:18:07 +0100

    Add specialField interface

---
+++ battle-model/src/main/java/pl/psi/specialfields/SpecialFieldIf.java
@@ -0,0 +1,13 @@
+package pl.psi.specialfields;
+
+public interface SpecialFieldIf {
+    boolean isMovePossible();
+    boolean isAttackPossible();
+
+    boolean isAttackable();
+    int getHp();
+    String getName();
+    int getDamage();
+
+    int getAmount();
+}
