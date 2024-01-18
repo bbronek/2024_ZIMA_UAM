bb69e8bb87d40b0d4b09fb9d840d8b17701bc782
bbronek <barbro214@gmail.com>
2024-01-18 21:34:53 +0100

    Add sample fields on board

--- battle-model/src/main/java/pl/psi/Board.java
+++ battle-model/src/main/java/pl/psi/Board.java
@@ -1,5 +1,6 @@
 package pl.psi;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +9,8 @@ import com.google.common.collect.HashBiMap;
 
 import pl.psi.creatures.Creature;
 import pl.psi.specialfields.SpecialField;
+import pl.psi.specialfields.SpecialFieldFactory;
+import pl.psi.specialfields.SpecialFieldsStatistics;
 
 /**
  * TODO: Describe this class (The first line - until the first dot - will interpret as the brief description).
@@ -16,11 +19,15 @@ public class Board
 {
     private static final int MAX_WITDH = 14;
     private final BiMap< Point, Creature > map = HashBiMap.create();
+    private final HashMap< Point, SpecialField> specialFieldsMap = new HashMap<Point, SpecialField>();
 
     public Board( final List< Creature > aCreatures1, final List< Creature > aCreatures2 )
     {
         addCreatures( aCreatures1, 0 );
         addCreatures( aCreatures2, MAX_WITDH );
+        specialFieldsMap.put(new Point(2, 3), SpecialFieldFactory.create(SpecialFieldsStatistics.LAVA));
+        specialFieldsMap.put(new Point(2, 5), SpecialFieldFactory.create(SpecialFieldsStatistics.ROCK));
+        specialFieldsMap.put(new Point(5, 5), SpecialFieldFactory.create(SpecialFieldsStatistics.GUARD));
     }
 
     private void addCreatures( final List< Creature > aCreatures, final int aXPosition )
